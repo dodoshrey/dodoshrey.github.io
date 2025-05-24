dbcd4d80b7c44cc29109d355ac6e63da141a4c24
dodoshrey <wahishrey@gmail.com>
2025-05-24 14:32:09 +0100

    updating config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: mode === 'production' ? '/' : '/',
+  plugins: [react()]
 }));
